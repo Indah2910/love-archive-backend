ded4f8d21d6d96e098317279edc93ff6f0125a62
Indah2910 <indhftryni23@gmail.com>
2026-07-19 22:42:06 +0700

    Make api/index.js self-contained to avoid bundling collision

--- api/index.js
+++ api/index.js
@@ -2,15 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import storyRoutes from "./routes/storyRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import galleryRoutes from "./routes/galleryRoutes.js";
-import memoryRoutes from "./routes/memoryRoutes.js";
-import guestbookRoutes from "./routes/guestbookRoutes.js";
-import loveLetterRoutes from "./routes/loveLetterRoutes.js";
-import playlistRoutes from "./routes/playlistRoutes.js";
-import bucketListRoutes from "./routes/bucketListRoutes.js";
-import profileRoutes from "./routes/profileRoutes.js";
+import storyRoutes from "../src/routes/storyRoutes.js";
+import authRoutes from "../src/routes/authRoutes.js";
+import galleryRoutes from "../src/routes/galleryRoutes.js";
+import memoryRoutes from "../src/routes/memoryRoutes.js";
+import guestbookRoutes from "../src/routes/guestbookRoutes.js";
+import loveLetterRoutes from "../src/routes/loveLetterRoutes.js";
+import playlistRoutes from "../src/routes/playlistRoutes.js";
+import bucketListRoutes from "../src/routes/bucketListRoutes.js";
+import profileRoutes from "../src/routes/profileRoutes.js";
 
 dotenv.config();
 
@@ -18,8 +18,6 @@ const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
-
-app.use("/uploads", express.static("src/uploads"));
 
 app.use("/api/story", storyRoutes);
 app.use("/api/auth", authRoutes);
@@ -32,7 +30,7 @@ app.use("/api/bucketlist", bucketListRoutes);
 app.use("/api/profiles", profileRoutes);
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Love Archive API jalan 💙" });
+  res.json({ message: "Love Archive API jalan" });
 });
 
 app.use((req, res) => {
